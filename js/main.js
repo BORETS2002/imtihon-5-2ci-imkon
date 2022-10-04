f61c362b1aa4848c2309f5a37165e0be82045339
BORETS2002 <BORETS2002@example.com>
2022-10-04 19:43:31 +0300

    finish page

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+$('.center').slick({
+  Infinity: true,
+  centerMode: true,
+  centerPadding: '0px',
+  slidesToShow: 3,
+  responsive: [
+  
+    {
+      breakpoint: 320,
+      settings: {
+        arrows: true,
+        centerMode: false,
+        centerPadding: '0px',
+        slidesToShow: 1
+      }
+    },
+
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: true,
+        centerMode: false,
+        centerPadding: '0px',
+        slidesToShow: 1
+      }
+    },
+  ]
+});
