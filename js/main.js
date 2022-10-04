780e58715724f58b70ad12a4892c9d3bd2155164
BORETS2002 <BORETS2002@example.com>
2022-10-04 22:17:05 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -19,11 +19,36 @@ $('.center').slick({
     {
       breakpoint: 768,
       settings: {
+  
         arrows: true,
         centerMode: false,
         centerPadding: '0px',
         slidesToShow: 1
       }
     },
+
+    {
+      breakpoint: 998,
+      settings: {
+  
+        arrows: true,
+        centerMode: false,
+        centerPadding: '0px',
+        slidesToShow: 1
+      }
+    },
+
+
+    {
+      breakpoint: 1200,
+      settings: {
+  
+        arrows: true,
+        centerMode: false,
+        centerPadding: '0px',
+        slidesToShow: 1
+      }
+    },
+
   ]
 });
